bdc58fb490445f2d8b8f2a2dd9bb80be19962dca
MagomedBolatov <maga8112002@gmail.com>
2024-08-25 03:13:24 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var=( 'a', 'b', 1, 2)
+print(immutable_var)
+#immutable_var['a']=m
+#изменения невозможны, так как кортеж не поддерживает изменения элементов
+mutable_list=[3, 4, 'h', 't']
+mutable_list [0]=6
+mutable_list [1]=7
+mutable_list [2]='r'
+mutable_list [3]='x'
+print(mutable_list)
